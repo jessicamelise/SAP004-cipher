33fd887eb8f733df2ad1a7af65c57abb841eaf5a
jessicamelise <jessica.melise@hotmail.com>
2020-03-29 12:55:57 -0300

    criando função encode e decode e usando o throw TypeError

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,20 @@
 const cipher = {
-  // ...
+  encode: function (numero, string) {
+    if (typeof (numero) != typeof (0)) {
+      throw new TypeError();
+    } else if (typeof (string) != typeof ("a")) {
+      throw new TypeError();
+    }
+  },
+
+  decode: function (numero, string) {
+    if (typeof (numero) != typeof (0)) {
+      throw new TypeError();
+    } else if (typeof (string) != typeof ("a")) {
+      throw new TypeError();
+    }
+  }
+
 };
 
 export default cipher;
