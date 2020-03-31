7f229c08ad5dd2199ece8ea8a6f38040951d09b9
jessicamelise <jessica.melise@hotmail.com>
2020-03-30 22:13:11 -0300

    Inicio do HTML e interação com index.js para cifrar mensagem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let takePhrase = document.getElementById("takePhrase");
+let offsetNumber = document.getElementById("offsetNumber");
+let boxToCipher = document.getElementById("boxToCipher");
+let boxCipherMessage = document.getElementById("boxCipherMessage");
+let receiveCipherPhrase = document.getElementById("receiveCipherPhrase");
+let encodeButton = document.getElementById("encodeButton");
+
+startDisplay();
+
+function startDisplay () {
+boxToCipher.style.display = "block";
+boxCipherMessage.style.display = "none";
+}
+
+encodeButton.addEventListener("click", encodeClick);
+
+
+function encodeClick () {
+    let value = parseInt(offsetNumber.value);
+    let codeMessage = cipher.encode(value, takePhrase.value);
+    receiveCipherPhrase.innerHTML = codeMessage;
+    boxCipherMessage.style.display = "block";
+}
+
+
+
+
